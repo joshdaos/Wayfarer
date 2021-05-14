cb03673c7224f151f1ea418a03b85e6c4acf9b7f
JoshDaos <joshuadaos1998@gmail.com>
2021-05-14 10:43:52 -0700

    back-end stuff

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,14 @@
 from django.db import models
-
+from django.db.models import Model, DateTimeField
+import time
 # Create your models here.
 
-
+class UserProfile(Model):
+    name = models.CharField(max_length=100)
+    currentcity = models.CharField(max_length=500)
+    join_date = models.DateTimeField(auto_now_add=True)
+    img = models.CharField(max_length=500)
+    def __str__(self):
+        return self.name
+    class Meta:
+        ordering = ['name']
