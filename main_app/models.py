6a0f1470c60952eb96fd1f3dd403b7e1fdd78d9a
kbaskerville <klbaskerille0520@gmail.com>
2021-05-14 09:35:53 -0700

    udpate

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class UserProfile(models.Model):
+
+    name = models.CharField(max_length=100)
+    currentcity = models.CharField(max_length=250)
+    join_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
