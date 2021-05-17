0dc519f6e692de35ca21043cffcc2741a87b3309
JoshDaos <joshuadaos1998@gmail.com>
2021-05-16 23:18:07 -0700

    merging

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', views.Home.as_view(), name="home"),
     path('profile/', views.UserProfileList.as_view(), name="profile_list"),
     path('profile/<int:pk>/', views.PostsDetail.as_view(), name="profile_detail"),
-    path('profile/<int:pk>/posts/new',views.PostsCreate.as_view(), name="posts_create"),
+    path('profile/<int:pk>/posts/new',views.PostsCreate.as_view(), name="profile_detail"),
     path('accounts/signup/', views.Signup.as_view(), name="signup"),
 ]
